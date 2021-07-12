325bd9aaabca4f59070c4b63fc77ebba72055ffb
Aleks <lexxus07@yandex.ru>
2021-07-12 17:55:29 +0300

    feat: practice ES6

--- lesson_3.js
+++ lesson_3.js
@@ -27,7 +27,7 @@ arr[7]();
 let fun = () => {
   console.log(this);
 };
-fun();
+// fun();
 
 let obj = {
   number: 5,
@@ -39,3 +39,47 @@ let obj = {
   }
 };
 obj.sayNumber();
+
+let btn = document.querySelector('button');
+
+btn.addEventListener('click', function () {
+  let show = () => {
+    console.log(this);
+  };
+  show();
+});
+
+function calcOrDouble(number, basis = 2) {
+  // basis = basis || 2; //ES5
+  console.log(number * basis);
+}
+calcOrDouble(3, 5);
+calcOrDouble(6);
+
+class Rectangle {
+  constructor(height, width = 20) {
+    this.height = height;
+    this.width = width;
+  }
+  calcArea() {
+    return this.height * this.width;
+  }
+}
+const square = new Rectangle(10, 10);
+console.log(square.calcArea());
+
+let video = ['youtube', 'vimeo', 'rutube'],
+  blogs = ['wordpress', 'livejournal', 'blogger'],
+  internet = [...video, ...blogs, 'vk', 'facebook'];
+
+console.log(internet);
+
+function log(a, b, c) {
+  console.log(a);
+  console.log(b);
+  console.log(c);
+  console.log(a + b + c);
+}
+let numbers = [2, 5, 7];
+
+log(...numbers);
