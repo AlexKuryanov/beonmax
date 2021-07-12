6d5e477a73dc6eab66316fe8990862d51ed44823
Aleks <lexxus07@yandex.ru>
2021-07-12 21:01:24 +0300

    refactor: compare my answer with teacher^s

--- es6.js
+++ es6.js
@@ -9,11 +9,11 @@ class Options {
   }
   createDiv(text) {
     let div = document.createElement('div');
-    document.body.append(div);
-    div.textContent = text;
+    document.body.append(div); // append-adds several elements, 
+    div.textContent = text; //  appendChild only one
 
     const textCss = () => {
-      div.style.height = this.height;
+      div.style.height = this.height; // can write in one line with incap
       div.style.width = this.width;
       div.style.backgroundColor = this.bg;
       div.style.fontSize = this.fontSize;
@@ -27,3 +27,25 @@ let newDiv = new Options('200px', '500px', 'lightgreen', '16px', 'center');
 newDiv.createDiv('created first div');
 let secondDiv = new Options('300px', '400px', 'pink', '28px', 'right');
 secondDiv.createDiv('created second div');
+
+// teachers code
+/* class Options {
+	constructor(height, width, bg, fontSize, textAlign) {
+		this.height = height;
+		this.width = width;
+		this.bg = bg;
+		this.fontSize = fontSize;
+		this.textAlign = textAlign;
+	}
+
+	createDiv() {
+		let elem = document.createElement('div');
+		document.body.appendChild(elem);
+		let param = `height:${this.height}px; width:${this.width}px; background-color:${this.bg}; font-size:${this.fontSize}px; text-align:${this.textAlign}`;
+		elem.style.cssText = param;
+	}
+}
+
+const item = new Options(300, 350, "red", 14, "center");
+
+item.createDiv();*/
