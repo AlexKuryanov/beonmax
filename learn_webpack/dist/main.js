508427e75a18dda8a70a7dc58586824842082e4c
AlexKuryanov <lexxus07@yandex.ru>
2021-07-18 19:29:29 +0300

    feat: learn how to plug in webpack without config

---
+++ learn_webpack/dist/main.js
@@ -0,0 +1,1 @@
+(()=>{var o={655:o=>{o.exports=function(){this.hello=function(){return"Hello!"},this.goodBye=function(){return"Goodbye!"}}}},e={};function r(t){var n=e[t];if(void 0!==n)return n.exports;var l=e[t]={exports:{}};return o[t](l,l.exports,r),l.exports}(()=>{let o=new(r(655));console.log(o.hello()),console.log(o.goodBye())})()})();
